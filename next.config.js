13c789a1442f273668f531ece16ef8b5826afa35
Dreiko98 <germanmallo04@gmail.com>
2025-06-21 16:48:33 +0200

    Configure Next.js for static export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // AppDir is enabled by default in Next.js 13.4+
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
